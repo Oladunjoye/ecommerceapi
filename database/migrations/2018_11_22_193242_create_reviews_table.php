a3ddf377c3e4d662f474b1c30ab2688f68872b67
joye <shonubij@gmail.com>
2018-11-22 21:32:56 +0100

    Migration created for models

--- database/migrations/2018_11_22_193242_create_reviews_table.php
+++ database/migrations/2018_11_22_193242_create_reviews_table.php
@@ -15,6 +15,10 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('product_id');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
