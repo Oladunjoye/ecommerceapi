7dee99acdce30921c09ef70267f032772578070f
joye <shonubij@gmail.com>
2018-11-23 09:00:19 +0100

    Product resource created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+
+      public function reviews(){
+    return $this->hasMany('App\Model\Review');
+}
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     //
+
+   public function product(){
+
+   	return $this->belongsTo('App\Model\Product');
+   } 
+     
 }
