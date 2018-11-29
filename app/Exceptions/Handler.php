76edbabcb19acc52afa1942f5a6c3fd40bf79857
joye <shonubij@gmail.com>
2018-11-29 01:18:38 +0100

    Exception refactoring

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,11 +5,13 @@ namespace App\Exceptions;
 use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
 
 class Handler extends ExceptionHandler
 {
+
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -51,24 +53,7 @@ class Handler extends ExceptionHandler
     {
         if($request->expectsJson()){
 
-            if($exception instanceof ModelNotFoundException){
-            return response()->json([
-
-                'errors' => 'Product Model not found'
-
-            ],Response::HTTP_NOT_FOUND);
-        }
-
-            if($exception instanceof NotFoundHttpException){
-
-
-                return response()->json([
-
-                'errors' => 'Incorrect route'
-
-            ],Response::HTTP_NOT_FOUND);
-
-            }
+           return $this->apiException($request, $exception);
         }
 
         
